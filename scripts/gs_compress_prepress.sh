05f9085f47536b01a9e22093b7825857a570e8c8
sandial1 <42413498+sandial1@users.noreply.github.com>
2026-01-21 08:19:05 -0500

    Feat base compression scripts

---
+++ scripts/gs_compress_prepress.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=prepress_compress.pdf ./FILENAME.pdf
---
+++ scripts/gs_compress_screen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=screen_compress.pdf ./FILENAME.pdf
