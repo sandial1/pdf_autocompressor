57f08bef65188cea03d4294856209f0788e80f1a
sandial1 <42413498+sandial1@users.noreply.github.com>
2026-01-21 08:32:53 -0500

    Feat script to move all files from raw to archive as part of the cleanup process

--- scripts/mv_all_raw_archive.sh
+++ scripts/mv_all_raw_archive.sh
@@ -5,4 +5,8 @@ PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 RAW_DIR="$PROJECT_ROOT/data/raw"
 ARCHIVE_DIR="$PROJECT_ROOT/data/archive"
 
-mv "$RAW_DIR"/*.pdf "$ARCHIVE_DIR"/ 2>/dev/null
+# Move all files from data/raw to data/archive
+#mv "$RAW_DIR"/*.pdf "$ARCHIVE_DIR"/ 2>/dev/null
+
+# Move all files, regardless of extension
+mv "$RAW_DIR"/* "$ARCHIVE_DIR"/ 2>/dev/null
